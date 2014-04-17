c6f26c42c3856c5e1fff52aaaa36641b34914b9f
Per Samuelsson <per@starcounter.com>
2014-04-17 12:45:57 +0200

    Implemented the first handler in the MongoDb PokerDemo application, allowing all data to be deleted.

--- PokerDemoAppMongoDb/Program.cs
+++ PokerDemoAppMongoDb/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using Starcounter;
+using MongoDB.Driver;
 
 namespace PokerDemoAppMongoDb {
+
+    #region Helper class
+    internal static class Mongo {
+        public static MongoDatabase Db;
+        public static void Init() {
+            var db = new MongoClient("mongodb://localhost").GetServer().GetDatabase("pokerdemo");
+            Mongo.Db = db;
+        }
+    }
+
+    #endregion
+
     class Program {
         static void Main() {
+            Mongo.Init();
 
+            Handle.DELETE("/all", () => {
+                Mongo.Db.GetCollection("Players").RemoveAll();
+                Mongo.Db.GetCollection("Accounts").RemoveAll();
+                return 200;
+            });
         }
     }
 }
