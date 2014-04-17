3a07d53201e2d53b672e22d071a6a609f5388dee
Per Samuelsson <per@starcounter.com>
2014-04-17 12:39:38 +0200

    Added first version of a domain model for the MongoDb poker demo application.

---
+++ PokerDemoAppMongoDb/Model.cs
@@ -0,0 +1,33 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace PokerDemoAppMongoDb {
+
+    public class Player {
+        public ObjectId Id { get; set; }
+        public int PlayerId { get; set; }
+        public string FullName { get; set; }
+        
+        [BsonIgnore]
+        public ICollection<Account> Accounts {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+
+    public class Account {
+        public ObjectId Id { get; set; }
+        public int AccountId { get; set; }
+        public int AccountType { get; set; }
+        public int Balance { get; set; }
+
+        [BsonIgnore]
+        public Player Player {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
