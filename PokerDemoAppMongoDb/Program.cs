324f26458ffe1db5ac3aab8907ecc6fb99da8908
Per Samuelsson <per@starcounter.com>
2014-04-17 13:41:15 +0200

    Implemented the PUT handler that supports adding new players to the MongoDb database in the PokerDemo.

--- PokerDemoAppMongoDb/Program.cs
+++ PokerDemoAppMongoDb/Program.cs
@@ -19,7 +19,21 @@ namespace PokerDemoAppMongoDb {
         static void Main() {
             Mongo.Init();
 
-            Handle.DELETE("/all", () => {
+            Handle.PUT(8082, "/players/{?}", (int playerId, PlayerAndAccounts json) => {
+                var player = new Player() { PlayerId = playerId, FullName = json.FullName };
+                Mongo.Db.GetCollection("Players").Insert(player);
+                foreach (var a in json.Accounts) {
+                    var account = new Account {
+                        AccountId = (int)a.AccountId,
+                        AccountType = (int)a.AccountType,
+                        Balance = (int)a.Balance,
+                        Player = player
+                    };
+                }
+                return 201;
+            });
+
+            Handle.DELETE(8082, "/all", () => {
                 Mongo.Db.GetCollection("Players").RemoveAll();
                 Mongo.Db.GetCollection("Accounts").RemoveAll();
                 return 200;
