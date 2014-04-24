59917a970bb1e6592babc6d31bb4a4a73923a16f
Per Samuelsson <per@starcounter.com>
2014-04-24 16:39:04 +0200

    Made complementing additions to the MongoDb PokerDemo data model to support transactional behavior (mainly a good-enough atomicy for the sake of this simple demo), not built in to MongoDb natively.

--- PokerDemoAppMongoDb/Model.cs
+++ PokerDemoAppMongoDb/Model.cs
@@ -16,7 +16,7 @@ namespace PokerDemoAppMongoDb {
         public IEnumerable<Account> Accounts {
             get {
                 var query = Query<Account>.EQ(a => a.PlayerObjectId, Id);
-                return Mongo.Db.GetCollection("Accounts").FindAs<Account>(query);
+                return Mongo.Db.Collection<Account>().FindAs<Account>(query);
             }
         }
     }
@@ -27,11 +27,28 @@ namespace PokerDemoAppMongoDb {
         public int AccountType { get; set; }
         public int Balance { get; set; }
         public ObjectId PlayerObjectId { get; set; }
+        public ObjectId[] PendingTransactions { get; set; }
 
         [BsonIgnore]
         public Player Player {
-            get { return Mongo.Db.GetCollection("Players").FindOneByIdAs<Player>(PlayerObjectId); }
+            get { return Mongo.Db.Collection<Player>().FindOneByIdAs<Player>(PlayerObjectId); }
             set { PlayerObjectId = value.Id; }
         }
+    }
+
+    public class AccountBalanceTransaction {
+        public static class States {
+            public const string Initial = "Initial";
+            public const string Pending = "Pending";
+            public const string Committed = "Committed";
+            public const string Done = "Done";
+            public const string Cancelling = "Cancelling";
+        }
+
+        public ObjectId Id { get; set; }
+        public ObjectId From { get; set; }
+        public ObjectId To { get; set; }
+        public int Amount { get; set; }
+        public string State { get; set; }
     }
 }
