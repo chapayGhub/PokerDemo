8c2b8ec5ebbbd8e23c4c7b096c9163c81b377adb
Per Samuelsson <per@starcounter.com>
2014-05-02 11:28:54 +0200

    Increased the maximum connection pool size in the MongoDb server application (from the default, 100, to 5000).

--- PokerDemoAppMongoDb/MongoDb.cs
+++ PokerDemoAppMongoDb/MongoDb.cs
@@ -10,8 +10,11 @@ namespace PokerDemoAppMongoDb {
         static Dictionary<Type, string> collectionNames = new Dictionary<Type, string>();
         public static MongoDatabase Db;
         public static void Init() {
-            var db = new MongoClient("mongodb://localhost").GetServer().GetDatabase("pokerdemo");
+            var client = new MongoClient("mongodb://localhost/?maxPoolSize=5000");
+            var db = client.GetServer().GetDatabase("pokerdemo");
             Mongo.Db = db;
+            Console.WriteLine(client.Settings);
+
             collectionNames.Add(typeof(Player), "Players");
             collectionNames.Add(typeof(Account), "Accounts");
             collectionNames.Add(typeof(AccountBalanceTransaction), "Transactions");
